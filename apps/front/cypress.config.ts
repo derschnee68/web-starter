a8e8368a03f34bb7f432a002f6325e0a64666503
Julien Schneider <julien.schneider.1991@gmail.com>
2022-12-24 15:03:54 +0100

    chore: cypress initial setup works

---
+++ apps/front/cypress.config.ts
@@ -0,0 +1,44 @@
+import { defineConfig } from 'cypress';
+import { cypressEsbuildPreprocessor } from 'cypress-esbuild-preprocessor';
+import { resolve } from 'path';
+import { config } from 'dotenv';
+import withCodeCoverage from '@cypress/code-coverage/task';
+
+config({ path: resolve(__dirname, '.env.test') });
+
+export default defineConfig({
+  projectId: 'e6u291',
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    env: {
+      archipel_token: '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',
+    },
+    supportFile: false,
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    setupNodeEvents(on, config) {
+      if (Boolean(config.env.coverage)) {
+        config = withCodeCoverage(on, config);
+      }
+      on(
+        'file:preprocessor',
+        cypressEsbuildPreprocessor({
+          esbuildOptions: {
+            tsconfig: resolve(__dirname, 'cypress', 'tsconfig.json'),
+            define: Object.entries(process.env).reduce(
+              (defineMap, [key, value]) => {
+                if (!key.startsWith('NEXT_PUBLIC')) return defineMap;
+
+                defineMap[`process.env.${key}`] = JSON.stringify(value);
+
+                return defineMap;
+              },
+              { global: 'window' } as Record<string, string>,
+            ),
+          },
+        }),
+      );
+      return config;
+    },
+  },
+});
