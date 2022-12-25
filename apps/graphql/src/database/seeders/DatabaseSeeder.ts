c4025259130a540db4327c785835a5e346833ba6
Julien Schneider <julien.schneider.1991@gmail.com>
2022-12-25 17:00:45 +0100

    chore(repo): can connect to mysql database

---
+++ apps/graphql/src/database/seeders/DatabaseSeeder.ts
@@ -0,0 +1,25 @@
+import type { EntityManager } from '@mikro-orm/core';
+import { Seeder } from '@mikro-orm/seeder';
+import User from '../entities/User';
+
+export class DatabaseSeeder extends Seeder {
+  private readonly team = ['user1', 'user2'];
+
+  async run(em: EntityManager) {
+    const existing = (await em.find(User, {}, { fields: ['id'] })).map(
+      (u) => u.id,
+    );
+
+    for (const uid of this.team) {
+      if (!existing.includes(uid)) {
+        em.create(User, {
+          id: uid,
+          email: `${uid}@mywebsite.ai`,
+          verifiedAt: new Date(),
+        });
+      } else {
+        await em.nativeUpdate(User, { id: uid }, { verifiedAt: new Date() });
+      }
+    }
+  }
+}
