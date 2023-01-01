3cb800c611b5aba3762b3572c8bb256f54cd4392
Julien Schneider <julien.schneider.1991@gmail.com>
2023-01-01 21:04:35 +0100

    feat(graphql): ignore peerDependencies

--- apps/front/src/pages/404.tsx
+++ apps/front/src/pages/404.tsx
@@ -9,7 +9,6 @@ export default function Custom404() {
       sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}
     >
       <Typography variant="h1">404</Typography>
-      <Typography variant="h2">Oops!</Typography>
       <Typography sx={{ mt: 2, mb: 4 }}>We are sorry, the page you were looking for does not exist anymore.</Typography>
       <Link href="/">
         <Button>Go back to homepage</Button>
