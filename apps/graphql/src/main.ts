8cae70f1798be0a603076dd835bd502c7acc7568
Julien Schneider <julien.schneider.1991@gmail.com>
2023-05-24 13:48:14 +0200

    Update main.ts

--- apps/graphql/src/main.ts
+++ apps/graphql/src/main.ts
@@ -1,12 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestLogger } from './logging/NestLogger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: new NestLogger(),
-  });
+  const app = await NestFactory.create(AppModule);
   await app.listen(3001);
 }
-
 void bootstrap();
